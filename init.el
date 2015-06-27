1ababf83c287eaafe7e1b73146a96d54276eab11
Christian Kroer <chrkroer@gmail.com>
2015-06-27 10:07:12 -0400

    added ESS

--- init.el
+++ init.el
@@ -34,7 +34,9 @@
     projectile
     volatile-highlights
     undo-tree
-    zygospore))
+    zygospore
+    ess
+    ))
 
 (defun install-packages ()
   "Install all required packages."
@@ -167,8 +169,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("73aaeaaef16976fd7b459ccd6fb3a2232a955b77738c19ad1a6f7b7a0821b2d6" default)))
- '(safe-local-variable-values (quote ((company-clang-arguments "-I/Users/ckroer/Documents/research/equilibrium_computation/mirror_prox/libdub/include" "/Users/ckroer/Documents/research/equilibrium_computation/mirror_prox/libdub/math/include")))))
+ '(custom-safe-themes
+   (quote
+    ("73aaeaaef16976fd7b459ccd6fb3a2232a955b77738c19ad1a6f7b7a0821b2d6" default)))
+ '(safe-local-variable-values
+   (quote
+    ((company-clang-arguments "-I/Users/ckroer/Documents/research/equilibrium_computation/mirror_prox/libdub/include" "/Users/ckroer/Documents/research/equilibrium_computation/mirror_prox/libdub/math/include")))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
